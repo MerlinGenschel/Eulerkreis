ee7246da7705c6b3af9c712ee1c72d4dc44032ba
alexej <alexej.f@posteo.de>
2019-12-13 15:04:43 +0100

    control

--- control.cpp
+++ control.cpp
@@ -1,6 +1,40 @@
 #include "control.h"
+#include "graph_net.h"
+
+#include<QEvent>
+#include<QMouseEvent>
+#include<QKeyEvent>
 
 control::control()
+{
+
+}
+
+// Eventfilter um die Ereignise von der Ansicht abzufangen : MVC: die Ereignise werden von der Steuerung verarbeitet
+bool control::eventFilter(QObject* /*watched*/, QEvent* event)
+{
+    switch(event->type()) // Bestimmen des Ereignistyps
+    {
+        // relevante Ereignistypen behandeln:
+        // cast auf speziellen Typ durchführen und die speziellen Event-Methoden aufrufen
+        case QEvent::MouseButtonPress:
+            zeichnen(dynamic_cast<QMouseEvent*>(event));
+            break;
+        case QEvent::MouseMove:
+            verschieben(dynamic_cast<QMouseEvent*>(event));
+            break;
+        default:
+            return false;
+    }
+    return event->isAccepted();
+}
+
+void control::zeichnen(QMouseEvent *event)
+{
+
+}
+
+void control::verschieben(QMouseEvent *event)
 {
 
 }
--- control.h
+++ control.h
@@ -1,11 +1,49 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 
+#include <QObject>
+#include<QPolygonF>
+#include<QPen>
 
-class control
+class QMouseEvent;
+
+enum class Mode;
+
+class control : public QObject
 {
+    Q_OBJECT
+
+    double rand = 0.1;
+    QPen   pen;
+    QPolygonF knoten;
+
+    bool linienDickeSkalieren = false;
 public:
+//*** Konstruktoren ***//
     control();
+
+    enum class Mode {zeichnen, verschieben};
+
+//*** get-Methoden ***//
+
+    Mode getMode() {return mode;}
+
+
+//*** set-Methoden ***//
+
+    void setMode(Mode mode);
+
+//*** Methoden ***//
+
+    // Eventfilter um die Ereignise von der Ansicht abzufangen
+    bool eventFilter(QObject* watched, QEvent* event) override;
+
+    void zeichnen(QMouseEvent* event);
+    void verschieben(QMouseEvent * event);
+
+private:
+    Mode mode = Mode::zeichnen;
+
 };
 
 #endif // CONTROL_H
